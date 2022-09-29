5e123f9343211bdce458597618bb052e1f65a8be
Marco De Toma <marco@keesdeboekhouder.nl>
2022-09-29 21:25:37 +0200

    map annotations

--- src/scraper/scraper.ts
+++ src/scraper/scraper.ts
@@ -1,16 +1,12 @@
 import {getFiles, readFile} from "../utils/fs.utils";
 import {log} from "../utils/log.utils";
-import {Annotation, ClassProperty, Table} from "../model/model";
+import {Annotation, AnnotationAttribute, ClassProperty, Table} from "../model/model";
 import {getFileContentSanitized} from "./sanitizer";
 import {getGroups} from "../utils/regex.util";
 
-const regexComponents = {
-    annotationAttributes: "(?:\\([ \\w=.,\"\\)]+)?"
-};
-
-const classFieldRegex = new RegExp("(?:@[\\w =,\"\\(\\)@ .]+)? private \\w+ \\w+;", 'g');
-const fieldAnnotation = new RegExp("@\\w+" + regexComponents.annotationAttributes, 'g');
-const captureFieldAnnotationRegex = new RegExp("(@\\w+" + regexComponents.annotationAttributes + ")", 'g');
+const classFieldRegex = new RegExp("(?:@[\\w =,\"()@ .]+)? private \\w+ \\w+;", 'g');
+const captureFieldAnnotationRegex = new RegExp("(@\\w+(?:\\([ \\w=.,\")]+)?)", 'g');
+const captureAnnotationNameAndAttribute = new RegExp("@(\\w+)(?:\\(([ \\w=.,\"]+)\\))?")
 
 const getProperties = (content: string): ClassProperty[] => {
     const properties = getGroups(content, classFieldRegex)
@@ -19,12 +15,17 @@ const getProperties = (content: string): ClassProperty[] => {
         const annotationsName = getGroups(property, captureFieldAnnotationRegex)
         log.debug("annotations name", annotationsName)
         const annotations = annotationsName.map(annotation => {
-            const attributesName = getGroups(annotation, fieldAnnotation)
-            log.debug("attributes name", attributesName)
-
+            const annotationParts = annotation.match(captureAnnotationNameAndAttribute)
+            const name = annotationParts![1]
+            const attributes = annotationParts![2]?.trim()
+            log.trace("ann name", name)
+            log.trace("ann attributes", attributes)
             return {
-                name: annotation,
-                attributes: []
+                name: name,
+                attributes: [{
+                    name: attributes,
+                    value: attributes
+                }]
             }
         })
         return {property, annotations}
@@ -45,7 +46,7 @@ export const scrape = (folder: string): Table[] => {
         log.trace(`content file sanitized ${javaFilePath}`, contentSanitized)
 
         const properties = getProperties(contentSanitized)
-        log.info(`properties ${javaFilePath}`, properties)
+        log.trace(`properties ${javaFilePath}`, properties)
 
         return {
             filePath: javaFilePath,
@@ -55,7 +56,7 @@ export const scrape = (folder: string): Table[] => {
         }
     });
 
-    log.trace(`entities`, entities)
+    log.debug(`entities`, JSON.stringify(entities, null, 4))
     log.debug(`num entities`, entities.length)
 
     return [];
