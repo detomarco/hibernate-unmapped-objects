6c98160c7f5fb6bdafa243fcdccb7e572a9afc51
detomarco <idp.detoma@gmail.com>
2022-10-04 23:43:05 +0200

    improve db connection

--- src/database/mysql.connection.ts
+++ src/database/mysql.connection.ts
@@ -1,5 +1,5 @@
 import { DbProperties } from '../model/model';
-import { DbTable } from './db.model';
+import { DatabaseConnection, DbTable } from './db.model';
 import { log } from '../utils/log.utils';
 import * as mysql from 'mysql2';
 
@@ -8,44 +8,50 @@ interface MysqlResultType {
     COLUMN_NAME: string
 }
 
-export const handleResults = (resultSet: MysqlResultType[]): DbTable[] => {
-    const resultsByTableName = resultSet.reduce((acc, result) => {
-        acc[result.TABLE_NAME] = acc[result.TABLE_NAME] || [];
-        acc[result.TABLE_NAME].push(result.COLUMN_NAME);
-        return acc;
-    }, {} as Record<string, string[]>);
+export class MysqlConnection extends DatabaseConnection {
 
-    return Object.entries(resultsByTableName)
-        .map(([name, columns]): DbTable => ({ name, columns }));
-};
+    private handleResults = (resultSet: MysqlResultType[]): DbTable[] => {
+        const resultsByTableName = resultSet.reduce((acc, result) => {
+            acc[result.TABLE_NAME] = acc[result.TABLE_NAME] || [];
+            acc[result.TABLE_NAME].push(result.COLUMN_NAME);
+            return acc;
+        }, {} as Record<string, string[]>);
 
-export const getMysqlColumns = (db: DbProperties): Promise<DbTable[]> => {
-    log.trace(`connection parameters: host:${db.host}, port:${db.port}, database: information_schema, user:${db.user}, password:****`);
-    const connection = mysql.createConnection({
-        host: db.host,
-        port: db.port,
-        user: db.user,
-        password: db.password,
-        database: 'information_schema'
-    });
+        return Object.entries(resultsByTableName)
+            .map(([name, columns]): DbTable => ({ name, columns }));
+    };
 
-    return new Promise((resolve, reject) => {
-        connection.query(
-            `select *
-             from information_schema.columns
-             where table_schema = '${db.schema}'`,
-            (err, results: MysqlResultType[]) => {
 
-                if (err === null) {
-                    log.trace('mysql results', results);
-                    const tables = handleResults(results);
-                    log.debug('mysql tables', tables);
-                    resolve(tables);
-                } else {
-                    log.error('mysql error', err);
-                    reject(err);
+    getTables(db: DbProperties): Promise<DbTable[]> {
+        log.trace(`connection parameters: host:${db.host}, port:${db.port}, database: information_schema, user:${db.user}, password:****`);
+        const connection = mysql.createConnection({
+            host: db.host,
+            port: db.port,
+            user: db.user,
+            password: db.password,
+            database: 'information_schema'
+        });
+
+        return new Promise((resolve, reject) => {
+            connection.query(
+                `select *
+                 from information_schema.columns
+                 where table_schema = '${db.schema}'`,
+                (err, results: MysqlResultType[]) => {
+
+                    if (err === null) {
+                        log.trace('mysql results', results);
+                        const tables = this.handleResults(results);
+                        log.debug('mysql tables', tables);
+                        resolve(tables);
+                    } else {
+                        log.error('mysql error', err);
+                        reject(err);
+                    }
                 }
-            }
-        );
-    });
-};
+            );
+        });
+    }
+
+}
+
